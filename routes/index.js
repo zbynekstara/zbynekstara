68cde52f49dfc5759cfbfd85d9d35178e5aefeb9
zbynekstara <zbynek.stara.internet@hotmail.com>
2015-05-26 11:01:16 +0400

    articles from mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
 var express  = require('express');
 var router   = express.Router();
 
+var MongoClient = require('mongodb').MongoClient;
+
 router.get('/', function(req, res) {
-    res.render('index', { title:'Home' });
+	MongoClient.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/test', function(err, db) {
+		if(!err);
+		db.collection('articles', function(err, collection) {
+			if (!err);
+			collection.find().sort({ "id":-1 }).limit(5).toArray(function(err, articles) {
+				if (!err);
+				db.close();
+
+				res.render('index', { title:'Home', articles:articles });
+			});
+		});
+	});	
 });
 
 module.exports = router;
