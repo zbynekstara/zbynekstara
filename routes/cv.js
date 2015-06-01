c73a4e661b935734989e81cda6dfe4c88e936c2e
zbynekstara <zbynek.stara.internet@hotmail.com>
2015-06-02 00:07:01 +0400

    cv

--- routes/cv.js
+++ routes/cv.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+var path = require('path');
+
 router.get('/cv', function(req, res) {
-	res.download('/cv/zbynek_stara_cv.pdf');
+	res.download(path.join(__dirname, '../public/cv', 'zbynek_stara_cv.pdf'), 'zbynek_stara_cv.pdf');
 });
 
 module.exports = router;
