5a65f37db1549a8c3444f55ec958a0677bf8946d
zbynekstara <zbynek.stara.internet@hotmail.com>
2019-03-19 01:29:03 +0100

    update cv, add cv in czech

--- routes/cv.js
+++ routes/cv.js
@@ -7,4 +7,8 @@ router.get('/cv', function(req, res) {
 	res.download(path.join(__dirname, '../public/cv', 'zbynek_stara_cv.pdf'), 'zbynek_stara_cv.pdf');
 });
 
+router.get('/cv_czech', function(req, res) {
+    res.download(path.join(__dirname, '../public/cv_czech', 'zbynek_stara_cv_czech.pdf'), 'zbynek_stara_cv_czech.pdf');
+});
+
 module.exports = router;
